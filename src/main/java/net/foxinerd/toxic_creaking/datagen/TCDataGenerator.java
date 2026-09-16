c58dceb2ce6ebd5254021cc5c92c56a3edd1e5c8
foxinerd <prytelewski64@gmail.com>
2026-09-16 14:20:07 -0400

    Implemented datagen

---
+++ src/main/java/net/foxinerd/toxic_creaking/datagen/TCDataGenerator.java
@@ -0,0 +1,10 @@
+package net.foxinerd.toxic_creaking.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class TCDataGenerator implements DataGeneratorEntrypoint {
+    @Override
+    public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+    }
+}
